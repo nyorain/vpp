790588cae12e04f04a0f6651da8f13a08913796b
nyorain <nyorain@gmail.com>
2016-07-22 22:08:13 +0200

    working on modelViewer

--- examples/modelViewer/modelViewer.cpp
+++ examples/modelViewer/modelViewer.cpp
@@ -2,6 +2,7 @@
 
 #include <nytl/vec.hpp>
 #include <nytl/stringParam.hpp>
+#include <nytl/convert.hpp>
 
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
@@ -9,36 +10,14 @@
 #include <assimp/cimport.h>
 
 
-//utility
-//to be patched into nytl
-template<typename T, typename O>
-struct Converter;
-
-template<typename T, typename O>
-auto convert(const O& other) -> decltype(Converter<T, O>::call(other))
-{
-	return Converter<T, O>::call(other);
-}
-
-template<typename O>
-class AutoCastable
-{
-public:
-	template<typename T>
-	operator T() const { return convert<T>(*data_); }
-
-public:
-	const O* data_;
-};
-
-//int a = convert(3.f);
-//int b = convert(&a); //not allowed
-template<typename O>
-AutoCastable<O> convert(const O& other) { return {&other}; }
-
 //specialization for aiVec -> nytlVec
+namespace nytl
+{
+
 template<> struct Converter<nytl::Vec3f, aiVector3D>
 	{ static nytl::Vec3f call(const aiVector3D& vec) { return {vec.x, vec.y, vec.z}; } };
+
+}
 
 //mesh loading
 struct Vertex
@@ -55,15 +34,13 @@ struct MeshInfo
 	unsigned int materialIndex;
 };
 
-class Mesh
+struct Mesh
 {
-public:
-
-protected:
-	vpp::Buffer buffer_;
+	MeshInfo info;
+	vpp::Buffer buffer;
 };
 
-std::vector<Mesh> loadMeshes(const nytl::StringParam& filename)
+std::vector<Mesh> loadMeshes(const vpp::Device& dev, const nytl::StringParam& filename)
 {
 	std::vector<Mesh> ret;
 	std::vector<MeshInfo> meshinfos;
@@ -89,26 +66,26 @@ std::vector<Mesh> loadMeshes(const nytl::StringParam& filename)
 		meshinfos.emplace_back();
 		auto& meshinfo = meshinfos.back();
 
-		meshinfo.materialIndex = mesh.mMaterialIndex;
-		meshinfo.vertices.resize(mesh.mNumVertices);
+		meshinfo.materialIndex = mesh->mMaterialIndex;
+		meshinfo.vertices.resize(mesh->mNumVertices);
 
-		for(std::size_t i(0); i < mesh.mNumVertices; ++i)
+		for(std::size_t i(0); i < mesh->mNumVertices; ++i)
 		{
 			auto& vertex = meshinfo.vertices[i];
 
-			vertex.position = convert(mesh.mVertices[i]);
-			vertex.normal = convert(mesh.mNormals[i]);
+			vertex.position = nytl::convert(mesh->mVertices[i]);
+			vertex.normal = nytl::convert(mesh->mNormals[i]);
 
-			if(mesh.HasTextureCoords(0))
+			if(mesh->HasTextureCoords(0))
 			{
-				vertex.texCoords = convert(mesh.mTextureCoords[0][i]);
+				vertex.texCoords = nytl::convert(mesh->mTextureCoords[0][i]);
 			}
 		}
 
-		meshinfo.indices.resize(mesh.mNumFaces * 3);
-		for(std::size_t i(0); i < mesh.mNumFaces; ++i)
+		meshinfo.indices.resize(mesh->mNumFaces * 3);
+		for(std::size_t i(0); i < mesh->mNumFaces; ++i)
 		{
-			auto& face = mesh.mFaces[i];
+			auto& face = mesh->mFaces[i];
 
 			meshinfo.indices[i * 3] = face.mIndices[0];
 			meshinfo.indices[i * 3 + 1] = face.mIndices[1];
@@ -116,56 +93,44 @@ std::vector<Mesh> loadMeshes(const nytl::StringParam& filename)
 		}
 
 		//buffer data
-		std::vector<std::uint8_t> bufferData;
+		auto verticesSize = meshinfo.vertices.size() * sizeof(Vertex);
+		auto indicesSize = meshinfo.indices.size() * sizeof(std::uint32_t);
+
+		std::vector<std::uint8_t> bufferData(verticesSize + indicesSize);
 		std::size_t offset = 0;
+
+		//first all vertices then all indices
+		std::memcpy(bufferData.data(), meshinfo.vertices.data(), verticesSize);
+		std::memcpy(bufferData.data() + verticesSize, meshinfo.indices.data(), indicesSize);
 
 		//create the buffer
 		vk::BufferCreateInfo bufferInfo;
 		bufferInfo.size = offset;
 		bufferInfo.usage = vk::BufferUsageBits::vertexBuffer | vk::BufferUsageBits::indexBuffer;
 
-		vpp::Buffer buffer(allocator, bufferInfo, vk::MemoryPropertyBits::deviceLocal);
+		vpp::Buffer buffer(dev, bufferInfo, vk::MemoryPropertyBits::deviceLocal);
 		vpp::fill140(buffer, vpp::raw(bufferData));
 
 		//mesh
-		Mesh mesh{std::move(vertices), std::move(indices), meshinfo.indices_.size()};
-		ret.push_back(std::move(mesh));
-	}
-
-
-	for(auto& meshinfo : meshinfos)
-	{
-
-		for(auto& vert : meshinfo.vertices)
-		{
-			for(auto type : vertexData)
-			{
-				float* data = nullptr;
-				std::size_t size = 0;
-
-				if(type == VertexData::position)
-				{
-					data = vert.position.data();
-					size = 3 * sizeof(float);
-				}
-				else if(type == VertexData::normal)
-				{
-					data = vert.normal.data();
-					size = 3 * sizeof(float);
-				}
-				else if(type == VertexData::textureCoords)
-				{
-					data = vert.textureCoords.data();
-					size = 2 * sizeof(float);
-				}
-
-				std::memcpy(vertexBufferData.data() + offset, data, size);
-				offset += size;
-			}
-		}
-
-		//vertices
+		Mesh mmesh {std::move(meshinfo), std::move(buffer)};
+		ret.push_back(std::move(mmesh));
 	}
 
 	return ret;
+}
+
+int main(int argc, char** argv)
+{
+	std::string filename = "model.obj";
+	initApp(app, [&](){ return std::make_unique<>(); });
+	mainLoop(app, [&](){
+		POINT pos;
+		GetCursorPos(&pos);
+		ScreenToClient(app.window, &pos);
+
+		auto vec = nytl::Vec2ui(pos.x, pos.y);
+		vec = nytl::clamp(vec, nytl::Vec2ui(0, 0), nytl::Vec2ui(app.width, app.height));
+
+		system.update(vec);
+	});
 }
---
+++ examples/nytl/convert.hpp
@@ -0,0 +1,74 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 nyorain
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+///\file
+///\brief Defines a utility to implement easy conversions without conversion operator.
+
+#pragma once
+
+#include <type_traits>
+
+namespace nytl
+{
+
+///Specialize this template with a static call function to enable explicit conversions
+///from type O to type T using the nytl::convert function.
+///By default it will just perform a static cast.
+///\sa nytl::convert
+template<typename T, typename O, typename = void>
+struct Converter;
+
+template<typename T, typename O>
+struct Converter<T, O, decltype(static_cast<T>(std::declval<O>()))>
+{
+	static O call(const T& other) { return static_cast<O>(other); }
+};
+
+///The convert function can be used to call specialization of the Converted struct.
+///This can be used to implement free conversion operations in a consistent way.
+template<typename T, typename O>
+auto convert(const O& other) -> decltype(Converter<T, O>::call(other))
+{
+	return Converter<T, O>::call(other);
+}
+
+///Utility wrapper around type O that will convert into any type to which the stored object
+///can be converted to.
+template<typename O>
+class AutoCastable
+{
+public:
+	template<typename T>
+	operator T() const { return convert<T>(*data_); }
+
+public:
+	const O* data_;
+};
+
+///Convert overload that only takes one template parameter which will be deduced.
+///Returns a wrapper object that will implicitly cast into any object into which the given
+///object of type O can be converted.
+template<typename O> AutoCastable<O> convert(const O& other) { return {&other}; }
+
+}
