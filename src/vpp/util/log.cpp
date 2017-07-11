71970f62757535c145d23640276c0aafcda9861d
nyorain <nyorain@gmail.com>
2017-07-11 19:45:31 +0200

    Remove unneeded macro

--- src/vpp/util/log.cpp
+++ src/vpp/util/log.cpp
@@ -2,5 +2,4 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt
 
-#define DLG_IMPLEMENTATION
 #include <dlg/dlg.cpp>
