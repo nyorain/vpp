01839d911cdc03cab894e443ce3cfde52260acab
nyorain <nyorain@gmail.com>
2017-10-14 11:24:46 +0200

    Simplify framebuffer

--- include/vpp/framebuffer.hpp
+++ include/vpp/framebuffer.hpp
@@ -5,53 +5,20 @@
 #pragma once
 
 #include <vpp/fwd.hpp>
-#include <vpp/device.hpp>
 #include <vpp/resource.hpp>
-#include <vpp/image.hpp>
 
 namespace vpp {
 
-// TODO: tsi at its worst. Rework it.
-// Maybe simple Framebuffer raii class and then an additional class
-// that knows (and maybe owns) its attachments?
-// use attachment variant like in renderer
-
-/// Vulkan Framebuffer consisting of multiple image attachments.
-/// Can be initialized with two step initialization.
-/// Stores its own size and is able to create and manage its own attachments but does
-/// not store any further information such as render pass compatibilty.
+/// Framebuffer wrapper. Stores no additional information.
 class Framebuffer : public ResourceHandle<vk::Framebuffer> {
 public:
-	using ExtAttachments = std::unordered_map<unsigned int, vk::ImageView>;
-	using AttachmentsInfo = nytl::Span<const ViewableImage::CreateInfo>;
-
-public:
 	Framebuffer() = default;
-	Framebuffer(const Device&, vk::RenderPass, const vk::Extent2D& size,
-		const AttachmentsInfo& attachments, const ExtAttachments& externalAttachments = {});
-	Framebuffer(const Device&, const vk::Extent2D& size, vk::Framebuffer);
 	Framebuffer(const Device&, const vk::FramebufferCreateInfo& info);
+	Framebuffer(const Device&, vk::Framebuffer);
 	~Framebuffer();
 
-	Framebuffer(Framebuffer&& rhs) noexcept { swap(*this, rhs); }
-	Framebuffer& operator=(Framebuffer rhs) noexcept { swap(*this, rhs); return *this; }
-
-	void create(const Device&, const vk::Extent2D& size, const AttachmentsInfo& info);
-
-	/// \exception std::logic_error if there are less view infos than image infos passed to create.
-	void init(vk::RenderPass rp, const AttachmentsInfo& info, const ExtAttachments& ext = {});
-	void init(vk::RenderPass rp, const std::vector<vk::ImageViewCreateInfo>& info,
-		const ExtAttachments& externalAttachments = {});
-
-	const std::vector<ViewableImage>& attachments() const { return attachments_; }
-	vk::Extent2D size() const;
-
-	friend void swap(Framebuffer& a, Framebuffer& b) noexcept;
-
-protected:
-	std::vector<ViewableImage> attachments_;
-	unsigned int width_ = 0;
-	unsigned int height_ = 0;
+	Framebuffer(Framebuffer&& rhs) noexcept;
+	Framebuffer& operator=(Framebuffer rhs) noexcept;
 };
 
 } // namespace vpp
--- src/vpp/framebuffer.cpp
+++ src/vpp/framebuffer.cpp
@@ -3,121 +3,36 @@
 // See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt
 
 #include <vpp/framebuffer.hpp>
-#include <vpp/memory.hpp>
-#include <vpp/renderPass.hpp>
 #include <vpp/vk.hpp>
-#include <vpp/util/span.hpp>
 
 namespace vpp {
 
-// Framebuffer
-Framebuffer::Framebuffer(const Device& dev, vk::RenderPass rp, const vk::Extent2D& size,
-	const AttachmentsInfo& attachments, const ExtAttachments& ext)
+Framebuffer::Framebuffer(const Device& dev, vk::Framebuffer framebuffer)
+	: ResourceHandle(dev, framebuffer)
 {
-	create(dev, size, attachments);
-	init(rp, attachments, ext);
-}
-
-Framebuffer::Framebuffer(const Device& dev, const vk::Extent2D& size, vk::Framebuffer framebuffer)
-	: ResourceHandle(dev, framebuffer), width_(size.width), height_(size.height)
-{
-
 }
 
 Framebuffer::Framebuffer(const Device& dev, const vk::FramebufferCreateInfo& info)
-	: ResourceHandle(dev)
+	: ResourceHandle(dev, vk::createFramebuffer(dev, info))
 {
-	width_ = info.width;
-	height_ = info.height;
-	handle_ = vk::createFramebuffer(dev, info);
 }
 
 Framebuffer::~Framebuffer()
 {
-	if(vkHandle()) vk::destroyFramebuffer(vkDevice(), vkHandle(), nullptr);
-}
-
-void swap(Framebuffer& a, Framebuffer& b) noexcept
-{
-	using std::swap;
-	using RH = ResourceHandle<vk::Framebuffer>;
-
-	swap(static_cast<RH&>(a), static_cast<RH&>(b));
-	swap(a.attachments_, b.attachments_);
-	swap(a.width_, b.width_);
-	swap(a.height_, b.height_);
-}
-
-void Framebuffer::create(const Device& dev, const vk::Extent2D& size,
-	const AttachmentsInfo& attachments)
-{
-	Resource::init(dev);
-	width_ = size.width;
-	height_ = size.height;
-
-	attachments_.reserve(attachments.size());
-	for(auto& attinfo : attachments) {
-		auto imgInfo = attinfo.imgInfo;
-		imgInfo.extent = {size.width, size.height, 1};
-
-		attachments_.emplace_back();
-		attachments_.back().create(device(), imgInfo, attinfo.memoryTypeBits);
+	if(vkHandle()) {
+		vk::destroyFramebuffer(vkDevice(), vkHandle(), nullptr);
 	}
 }
 
-void Framebuffer::init(vk::RenderPass rp, const AttachmentsInfo& attachments,
-	const ExtAttachments& extAttachments)
-{
-	std::vector<vk::ImageViewCreateInfo> info;
-	info.reserve(attachments.size());
-	for(auto& at : attachments) info.push_back(at.viewInfo);
-
-	init(rp, info, extAttachments);
+Framebuffer::Framebuffer(Framebuffer&& rhs) noexcept 
+{ 
+	swap(*this, rhs); 
 }
 
-void Framebuffer::init(vk::RenderPass rp, const std::vector<vk::ImageViewCreateInfo>& viewInfo,
-	const ExtAttachments& extAttachments)
-{
-	if(viewInfo.size() < attachments_.size())
-		throw std::logic_error("vpp::Framebuffer::init: to few viewInfos");
-
-	for(std::size_t i(0); i < attachments_.size(); ++i) attachments_[i].init(viewInfo[i]);
-
-	// framebuffer
-	// parse attachments
-	std::vector<vk::ImageView> attachments;
-	attachments.resize(attachments_.size() + extAttachments.size());
-
-	for(auto& extView : extAttachments) {
-		if(extView.first > attachments.size())
-			throw std::logic_error("vpp::Framebuffer: invalid external Attachment id given");
-
-		attachments[extView.first] = extView.second;
-	}
-
-	std::size_t currentID = 0;
-	for(auto& buf : attachments_) {
-		while(attachments[currentID])
-			++currentID;
-		attachments[currentID] = buf.vkImageView();
-		++currentID;
-	}
-
-	// createinfo
-	vk::FramebufferCreateInfo createInfo;
-	createInfo.renderPass = rp;
-	createInfo.attachmentCount = attachments.size();
-	createInfo.pAttachments = attachments.data();
-	createInfo.width = width_;
-	createInfo.height = height_;
-	createInfo.layers = 1; // TODO: should this be paramterized somehow?
-
-	handle_ = vk::createFramebuffer(vkDevice(), createInfo);
-}
-
-vk::Extent2D Framebuffer::size() const
-{
-	return {width_, height_};
+Framebuffer& Framebuffer::operator=(Framebuffer rhs) noexcept 
+{ 
+	swap(*this, rhs); 
+	return *this; 
 }
 
 } // namespace vpp
